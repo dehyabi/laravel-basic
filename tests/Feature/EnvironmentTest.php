a73880d701ab149e87a8fcacdc14326bef8bc233
dehyabi <dehyasvibe@gmail.com>
2022-12-18 12:19:38 +0700

    add HelloTest

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetEnv()
+    {
+        $youtube = env('YOUTUBE');
+	self::assertEquals('Dehya Qalbi', $youtube);
+    }
+}
